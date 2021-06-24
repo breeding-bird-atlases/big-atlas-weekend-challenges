91eb1edce4d44b47ac5818b2db94b7b851ddb667
birdnirdfoley <72263126+birdnirdfoley@users.noreply.github.com>
2021-06-24 16:10:04 -0400

    Update 24_big_atlas_weekend_challenges.R
    Rewrote state challenges to randomly select a winner rather than select the highest. Wrote the state-level contests. Added a summary dataframe to see the highest contributors for each contest. Will produce three csv files with the summarized results (challenge summaries, challenge winners, state winner).

--- 24_big_atlas_weekend_challenges.R
+++ 24_big_atlas_weekend_challenges.R
@@ -9,7 +9,6 @@
 
 library(dplyr)
 library(lubridate)
-library(suncalc)
 library(here)
 
 # year of event
@@ -19,6 +18,10 @@ year <- 2021
 baw <- read.csv(here("data", "ebird", "2_standardized",
                        paste0("big-atlas-weekend_", year, ".csv")))
 
+baw_checklists <- read.csv(here("data", "ebird", "2_standardized",
+                                paste0("big-atlas-weekend_checklists_", 
+                                       year, ".csv")))
+
 # pre-event data from eBird
 prebaw <- read.csv(here("data", "ebird", "2_standardized",
                           paste0("pre-big-atlas-weekend_", year, ".csv")))
@@ -27,7 +30,7 @@ prebaw <- read.csv(here("data", "ebird", "2_standardized",
 # needs to have name, email, user_id, project_code columns
 atlasers <- read.csv(here("data", "ebird", "2_standardized",
                           paste0("big-atlas-weekend_", year,
-                                 "atlasers.csv")))
+                                 "_atlasers.csv")))
 
 # list of all priority blocks from each region
 priority_blocks <- read.csv(here("data", "ebird", "2_standardized",
@@ -55,242 +58,286 @@ grassland_birds <- c("Upland Sandpiper",
                      "Bobolink",
                      "Eastern Meadowlark")
 
+priority_birds <- read.csv(here("data", "bba3", "priority_species.csv"))
+priority_birds <- priority_birds$common_name
+
 # project_codes
 me <- "EBIRD_ATL_ME"
 ny <- "EBIRD_ATL_NY"
 md <- "EBIRD_ATL_MD_DC"
 nc <- "EBIRD_ATL_NC"
 
-# applicable protocols
-protocols <- c("Traveling", "Stationary", "Area", "Random")
-
 # Data Prep -------------------------------------------------------------------
 
-# Calculate pre-event block effort
-# get rid of non-coded observations 
-prebaw_di <- prebaw %>% 
-  filter(breeding_bird_atlas_category %in% c("C2", "C3", "C4"))
+# Convert internal breeding codes to public codes
+int_codes <- read.csv(here("data", "ebird", "0_metadata", 
+                           "ebird_internal_codes.csv"))
 
-# get unique coded checklists
-prebaw_di <- prebaw_di[! duplicated(prebaw_di$checklist_id), ]
+baw <- left_join(baw, int_codes, by = c("breeding_code" = "INTERNAL"))
 
-# remove incomplete and nocturnal checklists
-prebaw_di <- prebaw_di %>% 
-  filter(all_species_reported == TRUE &
-           nocturnal == "diurnal")
+# edit the block codes to match partial blocks in ebird, which start with "o"
+priority_blocks$atlas_block <- gsub("o", "", priority_blocks$atlas_block)
+prebaw$block <- gsub("o", "", prebaw$block)
+baw$block <- gsub("o", "", baw$block)
 
-# Sum block effort
-prebaw_di <- prebaw_di %>% 
-  group_by(atlas_block) %>%
-  mutate(block_effort = sum(duration_minutes, na.rm = TRUE)) %>%
-  ungroup() %>%
-  select(atlas_block, block_effort)
+# NY has a few duplicate blocks so remove those
+priority_blocks <- distinct(priority_blocks)
 
-prebaw_di <- prebaw_di[! duplicated(prebaw_di$atlas_block), ]
+# Add all blocks into pre-event dataset so that 0-hour blocks are included
+prebaw <- full_join(prebaw, priority_blocks, 
+                    by = c("block" = "atlas_block")) %>%
+  distinct()
 
-prebaw_di <- full_join(prebaw_di, priority_blocks, by = "atlas_block")
+# Add pre-event dataset to event dataset
+baw <- full_join(baw, prebaw[, c("block", "block_effort", "priority")], 
+                 by = "block") %>%
+  distinct()
 
-baw <- full_join(baw, prebaw_di, by = "atlas_block")
+# add zero-obs checklist effort to event dataset
+baw <- full_join(baw, baw_checklists) %>%
+  distinct()
 
-# filter out non-portal checklists and inappropriate protocols
-baw <- baw %>%
-  filter(project_code %in% c(me, ny, md, nc) | 
-           protocol_type %in% protocols) %>%
-  select("checklist_id",
-         "global_unique_identifier",
-         "common_name",
-         "project_code",
-         "breeding_bird_atlas_code",
-         "breeding_bird_atlas_category",
-         "latitude",
-         "longitude",
-         "observation_date",
-         "time_observations_started",
-         "observer_id",
-         "duration_minutes",
-         "all_species_reported",
-         "atlas_block",
-         "priority",
-         "state",
-         "block_effort")
-
-# label nocturnal checklists
-# nocturnal = 20 min after sunset, 40 min before sunrise
-baw$observation_datetime <- as_datetime(paste(baw$observation_date, 
-                                              baw$time_observations_started), 
-                                        tz = "EST")
-
-baw[, (length(baw) + 1):(length(baw) + 7)] <-   
-  getSunlightTimes(data = data.frame(date = as.Date(baw$observation_date),
-                                     lat = baw$latitude,
-                                     lon = baw$longitude), 
-                   keep = c("sunrise", "nauticalDawn", 
-                            "sunset", "nauticalDusk"), 
-                   tz = "EST")
-
-baw[, c("date", "lat", "lon")] <- NULL
-
-# getSunlightTimes() doesn't account for daylight savings time
-## compensate for this
-### DST starts March 14 2021 02:00:00
-### DST ends November 07 2021 02:00:00
-dst <- as_datetime("2021-03-14 02:00:00", tz = "EST") %--% 
-  as_datetime("2021-11-07 02:00:00", tz = "EST")
-
-indx <- which(baw$observation_datetime %within% dst)
-baw[indx, c("sunrise")] <- baw[indx, c("sunrise")] + hours(1)
-baw[indx, c("nauticalDawn")] <- baw[indx, c("nauticalDawn")] + hours(1)
-baw[indx, c("sunset")] <- baw[indx, c("sunset")] + hours(1)
-baw[indx, c("nauticalDusk")] <- baw[indx, c("nauticalDusk")] + hours(1)
-
-rm(indx)
-
-# add columns for what ebird considers nocturnal
-baw$ebird_dawn <- baw$sunrise - hms::as_hms(40*60)
-baw$ebird_dusk <- baw$sunset + hms::as_hms(20*60)
-
-baw <- baw %>% 
-  rename(nautical_dawn = nauticalDawn, nautical_dusk = nauticalDusk) %>%
-  mutate(nocturnal = if_else(observation_datetime <= ebird_dawn | 
-                               observation_datetime >= ebird_dusk, 
-                             "nocturnal", "diurnal"))
-
-# change the checklist's designation to diurnal if it extends past dawn
-notna <-which(!is.na(baw$duration_minutes))
-indx <- which(baw$nocturnal[notna] == "nocturnal" &
-                ms(paste(baw$duration_minutes[notna], 0)) > 
-                abs(as_datetime(baw$ebird_dawn[notna]) -
-                      as_datetime(baw$observation_datetime[notna])))
-
-bba3[indx, "nocturnal"] <- "diurnal"
-
-rm(indx, notna)
+# Find new atlasers
+atlasers$first_submitted_date <- as_datetime(atlasers$first_submitted_date)
+new_atlasers <- filter(atlasers, 
+                       first_submitted_date > 
+                         as_datetime("2021-06-25 17:59:59"))
 
 # State Challenges ------------------------------------------------------------
 
 # o Nocturnal Hours -----------------------------------------------------------
 ## Complete nocturnal checklists with duration
-baw_noc <- baw[! duplicated(baw$checklist_id), ]
-
-baw_noc <- baw_noc %>%
-  group_by(observer_id, project_code) %>%
-  filter(nocturnal == "nocturnal" & 
+baw_noc <- baw[! duplicated(baw$sub_id), ] %>%
+  group_by(user_id, proj_id) %>%
+  filter((nocturnal == 1 & 
+           proj_id %in% c(ny, md, nc) &
            priority == "yes" &
-           all_species_reported == TRUE) %>%
-  summarize(duration = sum(duration_minutes, na.rm = TRUE)) %>%
-  group_by(project_code) %>%
-  slice_max(duration)
+           all_obs_reported == 1) |
+           (nocturnal == 1 &
+              proj_id %in% me &
+              priority == "yes"))
+
+winners <- baw_noc %>%
+  group_by(proj_id) %>%
+  slice_sample(n = 1) %>%
+  mutate(contest = "Nocturnal Hours")
+
+top_ten <- baw_noc %>%
+  summarize(challenge_value = sum(duration_hrs, na.rm = TRUE)) %>%
+  group_by(proj_id) %>%
+  slice_max(challenge_value, n = 10) %>%
+  mutate(contest = "Nocturnal Hours")
 
 # o Confirmed Codes -----------------------------------------------------------
 ## Complete checklists that have at least one species with confirmed code
 baw_c4 <- baw %>%
-  group_by(observer_id, project_code) %>%
-  filter(breeding_bird_atlas_category == "C4" &
+  group_by(user_id, proj_id) %>%
+  filter((category_code == "C4" &
+            proj_id %in% c(ny, md, nc) &
            priority == "yes" &
-           all_species_reported == TRUE) %>%
-  summarize(confirmations = length(breeding_bird_atlas_category)) %>%
-  group_by(project_code) %>%
-  slice_max(confirmations)
+           all_obs_reported == 1) |
+           (proj_id %in% me &
+              category_code == "C4" &
+              priority == "yes"))
 
-# o Confirmed Grassland Birds -------------------------------------------------
-## Complete checklists with confirmed grassland species
+top_ten <- baw_c4 %>%
+  summarize(challenge_value = length(category_code)) %>%
+  group_by(proj_id) %>%
+  slice_max(challenge_value, n = 10) %>%
+  mutate(contest = "Confirmed Codes") %>%
+  bind_rows(top_ten)
+
+baw_c4 <- baw_c4[! duplicated(baw_c4$sub_id), ]
+
+winners <- baw_c4 %>%
+  group_by(proj_id) %>%
+  slice_sample(n = 1) %>%
+  mutate(contest = "Confirmed Codes") %>%
+  bind_rows(winners)
+
+# o Confirmed Grassland/Priority Birds ----------------------------------------
+## Complete checklists with confirmed grassland/priority species
+## New York, Maine, and Maryland-DC only
 baw_grass <- baw %>%
-  group_by(observer_id, project_code) %>%
-  filter(common_name %in% grassland_birds &
-           breeding_bird_atlas_category == "C4" &
-           all_species_reported == TRUE) %>%
-  summarize(confirmations = length(breeding_bird_atlas_category)) %>%
-  group_by(project_code) %>%
-  slice_max(confirmations)
+  group_by(user_id, proj_id) %>%
+  filter((primary_com_name %in% grassland_birds &
+           proj_id %in% ny &
+           category_code == "C4" &
+           all_obs_reported == 1) |
+           (primary_com_name %in% grassland_birds &
+              proj_id %in% me &
+              category_code == "C4") |
+           (primary_com_name %in% priority_birds &
+              proj_id %in% md &
+              category_code == "C4" &
+              all_obs_reported == 1))
+
+top_ten <- baw_grass %>%
+  summarize(challenge_value = length(category_code)) %>%
+  group_by(proj_id) %>%
+  slice_max(challenge_value, n = 10) %>%
+  mutate(contest = "Priority Species") %>%
+  bind_rows(top_ten)
+
+baw_grass <- baw_grass[! duplicated(baw_grass$sub_id), ]
+
+winners <- baw_grass %>% 
+  group_by(proj_id) %>%
+  slice_sample(n = 1) %>%
+  mutate(contest = "Priority Species") %>%
+  bind_rows(winners)
 
 # o Checklists in <20 hr Blocks -----------------------------------------------
 ## Complete checklists longer than five minutes from blocks with <20 hrs of 
 ## diurnal effort.
-baw_20 <- baw[! duplicated(baw$checklist_id), ]
-
-baw_20 <- baw_20 %>%
-  group_by(observer_id, project_code) %>%
-  filter(block_effort <= 1200 &
-           duration_minutes >= 5 &
+baw_20 <- baw[! duplicated(baw$sub_id), ] %>%
+  group_by(user_id, proj_id) %>%
+  filter((block_effort <= 20 &
+           duration_hrs >= 0.83 &
+           proj_id %in% c(ny, md, nc) &
            priority == "yes" &
-           all_species_reported == TRUE) %>%
-  summarize(checklists = length(checklist_id)) %>%
-  group_by(project_code) %>%
-  slice_max(checklists)
+           all_obs_reported == 1) | 
+           (block_effort <= 20 &
+              duration_hrs >= 0.83 &
+              proj_id %in% me &
+              priority == "yes")) 
+
+winners <- baw_20 %>%
+  group_by(proj_id) %>%
+  slice_sample(n = 1) %>%
+  mutate(contest = "Low Effort Blocks") %>%
+  bind_rows(winners)
+
+top_ten <- baw_20 %>%
+  summarize(challenge_value = length(sub_id)) %>%
+  group_by(proj_id) %>%
+  slice_max(challenge_value, n = 10) %>%
+  mutate(contest = "Low Effort Blocks") %>%
+  bind_rows(top_ten)
 
 # o Checklists in 0 hr Blocks -------------------------------------------------
 ## Complete checklists longer than five minutes from blocks that had 0 hours of
 ## diurnal effort.
-baw_0 <- baw[! duplicated(baw$checklist_id), ]
-
-baw_0 <- baw_0 %>%
-  group_by(observer_id, project_code) %>%
-  filter(is.na(block_effort) &
-           duration_minutes >= 5 &
+baw_0 <- baw[! duplicated(baw$sub_id), ] %>%
+  group_by(user_id, proj_id) %>%
+  filter((is.na(block_effort) &
+           duration_hrs >= 0.83 &
            priority == "yes" &
-           all_species_reported == TRUE) %>%
-  summarize(checklists = length(checklist_id)) %>%
-  group_by(project_code) %>%
-  slice_max(checklists)
+           proj_id %in% c(ny, md, nc) &
+           all_obs_reported == 1) |
+           (is.na(block_effort) &
+              duration_hrs >= 0.83 &
+              priority == "yes" &
+              proj_id %in% me)) 
+
+winners <- baw_0 %>%
+  group_by(proj_id) %>%
+  slice_sample(n = 1) %>%
+  mutate(contest = "Zero Hr Blocks") %>%
+  bind_rows(winners)
+
+top_ten <- baw_0 %>%
+  summarize(challenge_value = length(sub_id)) %>%
+  group_by(proj_id) %>%
+  slice_max(challenge_value, n = 10) %>%
+  mutate(contest = "Zero Hr Blocks") %>%
+  bind_rows(top_ten)
 
 # o Atlas Checklists ----------------------------------------------------------
 ## Complete checklist with at least one coded species or nocturnal
 baw_atlas <- baw %>%
-  filter(breeding_bird_atlas_category %in% c("C2", "C3", "C4") | 
-           nocturnal == "nocturnal")
+  filter(category_code %in% c("C2", "C3", "C4") | nocturnal == 1)
 
-baw_atlas <- baw_atlas[! duplicated(baw_atlas$checklist_id), ]
+baw_atlas <- baw_atlas[! duplicated(baw_atlas$sub_id), ] %>%
+  group_by(user_id, proj_id) %>%
+  filter((all_obs_reported == 1 &
+            proj_id %in% c(ny, md, nc)) |
+           (proj_id %in% me)) 
 
-baw_atlas <- baw_atlas %>%
-  group_by(observer_id, project_code) %>%
-  filter(all_species_reported == TRUE) %>%
-  summarize(checklists = length(checklist_id)) %>%
-  group_by(project_code) %>%
-  slice_max(checklists)
+winners <- baw_atlas %>%
+  group_by(proj_id) %>%
+  slice_sample(n = 1) %>%
+  mutate(contest = "Atlas Checklists") %>%
+  bind_rows(winners)
+
+top_ten <- baw_atlas %>%
+  summarize(challenge_value = length(sub_id)) %>%
+  group_by(proj_id) %>%
+  slice_max(challenge_value, n = 10) %>%
+  mutate(contest = "Atlas Checklists") %>%
+  bind_rows(top_ten)
 
 # o First-time Checklists -----------------------------------------------------
 ## Any atlaser who contributes their first checklist
-baw_first <- anti_join(baw, atlasers, by = c("observer_id" = "user_id"))
+baw_first <- anti_join(baw, new_atlasers, by = c("observer_id" = "user_id"))
 
-baw_first <- baw_first %>%
-  group_by(project_code) %>%
-  slice_sample(n = 1)
+winners <- baw_first %>%
+  group_by(proj_id) %>%
+  slice_sample(n = 1) %>%
+  mutate(contest = "New Atlaser") %>%
+  bind_rows(winners)
 
-# Inter-state Individual Challenges -------------------------------------------
+write.csv(winners, here("output", paste0("big-atlas-weekend_winners_",
+                                         year, ".csv")),
+          row.names = FALSE)
 
-# o Nocturnal Hours -----------------------------------------------------------
-baw_noc_all <- baw_noc %>% 
-  ungroup() %>% 
-  slice_max(duration)
-
-# o Confirmed Codes -----------------------------------------------------------
-baw_c4_all <- baw_c4 %>%
-  ungroup() %>%
-  slice_max(confirmations)
-
-# o Confirmed Grassland Birds -------------------------------------------------
-baw_grass_all <- baw_grass %>%
-  ungroup() %>%
-  slice_max(confirmations)
-
-# o Checklists in <20 hr Blocks -----------------------------------------------
-baw_20_all <- baw_20 %>%
-  ungroup() %>%
-  slice_max(checklists)
-
-# o Checklists in 0 hr Blocks -------------------------------------------------
-baw_0_all <- baw_0 %>%
-  ungroup() %>%
-  slice_max(checklists)
-
-# o Atlas Checklists ----------------------------------------------------------
-baw_atlas_all <- baw_atlas %>%
-  ungroup() %>%
-  slice_max(checklists)
-
-# o First-time Checklists -----------------------------------------------------
-baw_first_all <- baw_first %>%
-  ungroup() %>%
-  slice_sample(n = 1)
+write.csv(top_ten, here("output", paste0("big-atlas-weekend_summary_",
+                                         year, ".csv")),
+          row.names = FALSE)
 
 # Inter-state Challenges ------------------------------------------------------
+
+# o Nocturnal Hours -----------------------------------------------------------
+state_winner <- baw[! duplicated(baw$sub_id), ] %>%
+  filter(nocturnal == 1 & 
+           priority == "yes" & 
+           all_obs_reported == 1) %>%
+  group_by(proj_id) %>%
+  summarize(challenge_value = (sum(duration_hrs, na.rm = TRUE)) / 
+              (length(unique(user_id)))) %>%
+  mutate(contest = "Nocturnal Hours")
+
+# o Confirmed Codes -----------------------------------------------------------
+num_priority_blocks <- priority_blocks %>% 
+  filter(priority == "yes") %>% 
+  group_by(state) %>% 
+  summarize(num_blocks = length(unique(atlas_block)))
+
+state_winner <- baw %>%
+  filter(category_code == "C4" & 
+           priority == "yes" & 
+           all_obs_reported == 1) %>%
+  mutate(num_blocks = case_when(
+    proj_id == me ~ 
+      num_priority_blocks$num_blocks[num_priority_blocks$state == 
+                                       "Maine"],
+    proj_id == ny ~
+      num_priority_blocks$num_blocks[num_priority_blocks$state == 
+                                       "New York"],
+    proj_id == md ~
+      num_priority_blocks$num_blocks[num_priority_blocks$state == 
+                                       "Maryland-DC"],
+    proj_id == nc ~
+      num_priority_blocks$num_blocks[num_priority_blocks$state == 
+                                       "North Carolina"],
+    TRUE ~ NA_integer_
+  )) %>%
+  group_by(proj_id) %>%
+  summarize(challenge_value = length(category_code) / unique(num_blocks)) %>%
+  mutate(contest = "Confirmed Codes") %>%
+  bind_rows(state_winner)
+
+# o Atlas Checklists ----------------------------------------------------------
+baw_atlas_state <- baw %>%
+  filter((category_code %in% c("C2", "C3", "C4") | nocturnal == 1) &
+          all_obs_reported == 1) 
+
+state_winner <- baw_atlas_state[! duplicated(baw_atlas_state$sub_id), ] %>%
+  group_by(proj_id) %>%
+  summarize(challenge_value = length(sub_id) / length(unique(user_id))) %>%
+  mutate(contest = "Atlas Checklists") %>%
+  bind_rows(state_winner)
+
+write.csv(state_winner, here("output", paste0("big-atlas-weekend_state-", 
+                                              "winner_", year, ".csv")),
+          row.names = FALSE)
